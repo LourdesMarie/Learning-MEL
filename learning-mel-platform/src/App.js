ba89a4a486c5386dc370d6e4a69922e89fd99217
LourdesMarie <luliorlando14@gmail.com>
2025-05-30 11:06:12 +0200

    Add files via upload

---
+++ learning-mel-platform/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Learning MEL Platform</h1>
+      <p>This is a working starter for deployment on Vercel.</p>
+    </div>
+  );
+}
+
+export default App;
