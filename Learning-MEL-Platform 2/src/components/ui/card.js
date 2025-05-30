79e93d9c2040a4ff66e190202be33cd98c5eabc5
LourdesMarie <luliorlando14@gmail.com>
2025-05-30 11:06:43 +0200

    Delete Learning-MEL-Platform 2 directory

--- Learning-MEL-Platform 2/src/components/ui/card.js
+++
@@ -1,2 +0,0 @@
-export function Card({ children }) { return <div>{children}</div>; }
-export function CardContent({ children }) { return <div>{children}</div>; }
--- Learning-MEL-Platform 2/src/components/ui/select.js
+++
@@ -1,4 +0,0 @@
-export function Select({ onValueChange, children }) { return <div>{children}</div>; }
-export function SelectTrigger({ children }) { return <button>{children}</button>; }
-export function SelectContent({ children }) { return <div>{children}</div>; }
-export function SelectItem({ value, children }) { return <div>{children}</div>; }
